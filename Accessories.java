d33b27da9f89ffa8f41a99bbc34edb13b7459db7
AshwiniSulibhavi <ashwinis.xworkz@gmail.com>
2021-09-05 11:57:00 +0530

    abc

---
+++ Accessories.java
@@ -0,0 +1,11 @@
+class Accessories{
+
+public static void main(String a[]){
+String accessoryItems="Diamond Necklace";
+int      price=35000;
+boolean isPure=true;
+System.out.println(accessoryItems);
+System.out.println(price);
+System.out.println(isPure);
+}
+}
